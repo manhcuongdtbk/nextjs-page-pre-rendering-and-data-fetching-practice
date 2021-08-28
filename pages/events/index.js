a9f1f83c88a011ca3a45a9ffdac6d7c1398d73fa
manhcuongdtbk <manhcuongdtbk@gmail.com>
2021-08-28 19:14:10 +0700

    Adding ISR to the events index page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
-import {getAllEvents} from "../../dummy-data"
+import {getAllEvents} from "../../helpers/api-utils"
 import EventList from "../../components/events/event-list"
 import EventsSearch from "../../components/events/events-search"
 import {Fragment} from "react"
 import {useRouter} from "next/router"
 
-function EventsIndexPage() {
+function EventsIndexPage(props) {
   const router = useRouter()
-  const events = getAllEvents()
+  const {events} = props
 
   function findEventsHandler(year, month) {
     const fullPath = `events/${year}/${month}`
@@ -19,6 +19,17 @@ function EventsIndexPage() {
       <EventList items={events}/>
     </Fragment>
   )
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents()
+
+  return {
+    props: {
+      events: events
+    },
+    revalidate: 60
+  }
 }
 
 export default EventsIndexPage
