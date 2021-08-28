e7c7ce12bf7a6246b1cb98e371a02da63500a473
manhcuongdtbk <manhcuongdtbk@gmail.com>
2021-08-28 18:41:12 +0700

    Adding SSG on the Home page

---
+++ helpers/api-utils.js
@@ -0,0 +1,21 @@
+export async function getAllEvents() {
+  // nppradfp is short for nextjs-page-pre-rendering-and-data-fetching-practice which is the name of this project
+  // Change the URL below to whatever backend api you use to return all events
+  const response = await fetch('https://nppradfp-default-rtdb.asia-southeast1.firebasedatabase.app/events.json')
+  const data = await response.json()
+  const events = []
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key]
+    })
+  }
+
+  return events
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents()
+  return allEvents.filter((event) => event.isFeatured)
+}
